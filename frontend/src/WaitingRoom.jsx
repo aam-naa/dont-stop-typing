6582167012cd1fb85c764de0e931bd175101b689
Chloe Lim <cheun0406@gmail.com>
2026-08-22 20:42:25 +1000

    increase timeout

--- frontend/src/WaitingRoom.jsx
+++ frontend/src/WaitingRoom.jsx
@@ -29,7 +29,7 @@ const WaitingRoom = () => {
       }
       if (data.type === "all_connected") {
         // TODO: navigate to the actual game screen once everyone's in
-        setTimeout(3000)
+        setTimeout(10000)
         console.log("timed out for 3000")
         navigate("/editor");
       }
