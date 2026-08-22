ac6f26c4bf0f45bcc2a3f77334da341958772227
Chloe Lim <cheun0406@gmail.com>
2026-08-22 15:59:55 +1000

    working local

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import Lobby from './Lobby.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Editor from './Editor.jsx'
+import WaitingRoom from './WaitingRoom.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,7 +14,6 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App/>} />
         <Route path="/lobby" element={<Lobby/>} />
         <Route path ="/editor" element={<Editor/>} />
-        <Route path="/waiting_room" element={<WaitingRoom />} />
         <Route path="/room/:roomId" element={<WaitingRoom/>} />
       </Routes>
     </BrowserRouter>
